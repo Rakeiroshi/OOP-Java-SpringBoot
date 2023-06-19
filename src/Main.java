9df81153b361b086d69704b255091cb29ec4f100
Giuseppe_Gravante <g.gravante88@gmail.com>
2023-06-19 10:58:39 +0200

    Esercizio 1 terminato

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import models.Studente;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Studente mia = new Studente("Mia");
+        mia.addGrade(29);
+        mia.addGrade(29);
+        mia.addGrade(28);
+        System.out.println("La studentessa " + mia.getNome() + " ha una media di: " + mia.getAverageGrade());
+
+        Studente eva = new Studente("Eva");
+        eva.addGrade(25);
+        eva.addGrade(30);
+        eva.addGrade(22);
+        System.out.println("La studentessa " + eva.getNome() + " ha una media di: " + eva.getAverageGrade());
     }
+
 }
---
+++ src/models/Studente.java
@@ -0,0 +1,32 @@
+package models;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Studente {
+    String nome;
+    List<Integer> grades = new ArrayList<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Studente(String nome) {
+        this.nome = nome;
+    }
+
+    public void addGrade(int voto){
+        grades.add(voto);
+    }
+
+    public double getAverageGrade(){
+        int somma = 0;
+        int num_voti = 0;
+
+        for( double i: grades) {
+            somma += i;
+            num_voti++;
+        }
+        double media = (double) somma / num_voti;
+        return media;
+        }
+}
