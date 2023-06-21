4ff65a1149434ecfd8db24c21af75977a428f8b6
Giuseppe_Gravante <g.gravante88@gmail.com>
2023-06-21 10:04:53 +0200

    Example maps

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("HAshMap");
+        Map<Integer,String> caffe = new HashMap<>(); //la mappa può dare gli elementi in modo casuale
+        caffe.put(1, "Barbone");
+        caffe.put(2, "lavazza");
+        caffe.put(3, "Brazil");
+        caffe.put(4, "Segafredo");
+        System.out.println(caffe);
+
+        System.out.println("TreeMap");
+        Map<String,String> caffeb= new TreeMap<>(); //la mappa può dare gli elementi in modo alfabetico
+        caffeb.put("Luca", "Barbone");
+        caffeb.put("Ezechiel", "lavazza");
+        caffeb.put("Matheus", "Brazil");
+        caffeb.put("Baraka", "Segafredo");
+        System.out.println(caffeb);
+
+
     }
 }
