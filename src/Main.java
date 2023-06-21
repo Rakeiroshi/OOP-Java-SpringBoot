86d6bc667a4675738e33e9c53fadb9ffc48fb454
Giuseppe_Gravante <g.gravante88@gmail.com>
2023-06-21 09:42:20 +0200

    esempi d'uso public, private, protected

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import models.Car;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Car auto = new Car(
+                "Enzo",
+                "Ferrari",
+                2002
+        );
+        System.out.println(auto.model); //Errore: model ha un accesso privato
+        System.out.println(auto.manufacturer); //Errore manufacturer è protetto
+        System.out.println(auto.year); //2002
+}
+    class Car {
+        private String model;
+        protected String manufacturer;
+        public int year;
+
+        public Car(String model, String manufacturer, int year) {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.year = year;
+        }
 }
