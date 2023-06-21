96e098fdd48b41cec4cc8cfead860de0f3a470d2
Giuseppe_Gravante <g.gravante88@gmail.com>
2023-06-21 12:53:59 +0200

    Esercizio veicoli

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Car ferrari = new Car(
                 318);
-        Motorcycle motorcycle = new Motorcycle(
+        Motorcycle vespa = new Motorcycle(
                 45);
 
         System.out.println("La velocità massima della Ferrari SF23: " + ferrari.getSpeed() + "km/h");
         System.out.println("Numero di ruote: " + ferrari.getNumberOfTires());
         System.out.println("*******************************************************");
-        System.out.println("La velocità della vespa 50 (non truccata): " + motorcycle.getSpeed() + "km/h");
-        System.out.println("Numero di ruote: " + motorcycle.getNumberOfTires());
+        System.out.println("La velocità della vespa 50 (non truccata): " + vespa.getSpeed() + "km/h");
+        System.out.println("Numero di ruote: " + vespa.getNumberOfTires());
     }
 }
