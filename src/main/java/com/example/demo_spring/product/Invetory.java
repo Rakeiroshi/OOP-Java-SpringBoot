45068a7bcd6ec2afcdb80057670d8ab55f1ac3d6
Giuseppe_Gravante <g.gravante88@gmail.com>
2023-06-26 11:24:19 +0200

    demo spring

---
+++ src/main/java/com/example/demo_spring/product/Invetory.java
@@ -0,0 +1,10 @@
+package com.example.demo_spring.product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Invetory {
+    private Long id;
+    private String name;
+    private List<Long> prodotti = new ArrayList<>();
+}
